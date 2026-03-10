03c0097b2795ef293730a244d07d8a17740586c3
José Muñoz <jdmg93@gmail.com>
2026-03-10 14:06:20 -0600

    feature: streaming mode toggle logic

--- src/lib/hue.functions.ts
+++ src/lib/hue.functions.ts
@@ -3,13 +3,15 @@ import type { Axios } from 'axios';
 import { createAxiosWithLookup } from './axios';
 import { listVideoInputs } from './capture.server';
 import { discover, register } from "./hue.server"
+import { StreamingClient } from './StreamingClient.server';
 import type { EntertainmentArea, HueBridgeNetworkDevice, HueBridgeRegistration } from "./types"
 
 let axios: Axios;
+let streamer: StreamingClient;
+let bridgeData: HueBridgeRegistration;
 
 export const getAvailableBridges = createServerFn().handler(async () => {
   const devices = await discover();
-  console.log("fetched the following bridges: ", devices)
   return devices ?? []
 })
 
@@ -18,6 +20,12 @@ export const registerWithBridge = createServerFn()
   .handler(async ({ data }) => {
     const credentials = await register(data.ip, "hue-hdmi-sync")
     axios = createAxiosWithLookup(data.id, data.ip, credentials)
+    bridgeData = {
+      id: data.id,
+      ip: data.ip,
+      name: data.name,
+      credentials,
+    }
 
     return credentials
   })
@@ -26,6 +34,7 @@ export const initializeBridge = createServerFn()
   .inputValidator((data: HueBridgeRegistration) => data)
   .handler(async ({ data }) => {
     axios = createAxiosWithLookup(data.id, data.ip, data.credentials)
+    bridgeData = { ...data }
   })
 
 export const getEntertainmentAreas = createServerFn().handler(async () => {
@@ -35,24 +44,58 @@ export const getEntertainmentAreas = createServerFn().handler(async () => {
   return wrapper.data as EntertainmentArea[];
 })
 
+const _getEntertainmentArea = async (id: string) => {
+  const response = await axios.get(`/entertainment_configuration/${id}`)
+  const wrapper = await response.data
+
+  return wrapper.data[0] as EntertainmentArea;
+}
+
 export const getEntertainmentArea = createServerFn()
   .inputValidator((data: { id: string }) => data)
   .handler(async ({ data }) => {
-    const response = await axios.get(`/entertainment_configuration/${data.id}`)
-    const wrapper = await response.data
-
-    return wrapper.data as EntertainmentArea[];
+    return _getEntertainmentArea(data.id)
   })
+
+const _updateEntertainmentArea = async (id: string, updates: Partial<EntertainmentArea>) => {
+  const response = await axios.put(`/entertainment_configuration/${id}`, updates)
+  const wrapper = await response.data
+
+  return wrapper.data;
+}
 
 export const updateEntertainmentArea = createServerFn()
   .inputValidator((data: { id: string, updates: Partial<EntertainmentArea> }) => data)
   .handler(async ({ data }) => {
-    const response = await axios.put(`/entertainment_configuration/${data.id}`, data.updates)
-    const wrapper = await response.data
-
-    return wrapper.data;
+    return _updateEntertainmentArea(data.id, data.updates)
   })
 
-export const getVideoInputs = createServerFn().handler(async () => {
+export const getVideoInputs = createServerFn().handler(() => {
   return listVideoInputs()
 })
+
+export const startStreaming = createServerFn()
+  .inputValidator((data: { id: string }) => data)
+  .handler(async ({ data }) => {
+    console.log("starting the stream..")
+    if (!streamer) {
+      streamer = new StreamingClient(bridgeData.ip, bridgeData.credentials, _updateEntertainmentArea)
+      console.log("initialized streaming client")
+    }
+    
+    console.log("retrieving the entertainment area config..")
+    const selectedArea = await _getEntertainmentArea(data.id)
+    console.log("fethed the entertainment area: ", selectedArea)
+    
+    streamer.start(selectedArea)
+    console.log("enabled stream mode on the entertainment area")
+    // start the CVWorker or something
+  })
+
+export const stopStreaming = createServerFn()
+  .handler(async () => {
+    if (streamer?.isStreaming) {
+      console.log("stopping the stream!")
+      streamer.stop();
+    }
+  })
